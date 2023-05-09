a967a0f367734e9696f1bc77d4b4abe11ed69d88
jaymi <22036150@myrp.edu.sg>
2023-05-09 17:18:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22036150/billplease/MainActivity.java
@@ -0,0 +1,118 @@
+package sg.edu.rp.c346.id22036150.billplease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText amtInput;
+    EditText paxInput;
+    ToggleButton SVC;
+    ToggleButton GST;
+    EditText discInput;
+    RadioGroup paymentRG;
+    Button split;
+    Button reset;
+
+    TextView totalBill;
+    TextView payEach;
+    double discount = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amtInput = findViewById(R.id.amtInput);
+        paxInput = findViewById(R.id.paxInput);
+        SVC = findViewById(R.id.calcSVC);
+        GST = findViewById(R.id.calcGST);
+        discInput = findViewById(R.id.discInput);
+        paymentRG = findViewById(R.id.payRadioGroup);
+        split = findViewById(R.id.splitButton);
+        reset = findViewById(R.id.resetButton);
+        totalBill = findViewById(R.id.totalBill);
+        payEach = findViewById(R.id.eachPayment);
+
+
+
+
+
+            split.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint({"SetTextI18n", "DefaultLocale"})
+                @Override
+                public void onClick(View view) {
+                    // Add your code for the action
+
+                    double origBill = Double.parseDouble(amtInput.getText().toString());
+                    double finalBill;
+
+
+                    if ((!SVC.isChecked()) && (!GST.isChecked())) {
+                        finalBill = origBill;
+                    } else if ((SVC.isChecked()) && (!GST.isChecked())) {
+                        finalBill = origBill * 1.1;
+                    } else if ((!SVC.isChecked()) && (GST.isChecked())) {
+                        finalBill = origBill * 1.08;
+                    } else {
+                        finalBill = origBill * 1.18;
+                    }
+
+
+                    if ((discInput.getText().toString().trim().length()) != 0) {
+                        discount = Double.parseDouble(discInput.getText().toString());
+                        finalBill *= (1 - discount / 100);
+                    }
+
+
+                    totalBill.setText("Total Bill: $" + String.format("%.2f", finalBill));
+
+                    String paymentMode = " in cash";
+                    if (paymentRG.getCheckedRadioButtonId() == R.id.paymentPayNow) {
+                        paymentMode = " via PayNow to 912345678";
+                    }
+
+                    int noOfPax = Integer.parseInt(paxInput.getText().toString());
+                    if (noOfPax != 1) {
+                        payEach.setText("Each Pays: $" + String.format("%.2f", (finalBill / noOfPax)) + paymentMode);
+                    } else {
+                        payEach.setText("Each Pays: $" + finalBill + paymentMode);
+                    }
+
+
+
+
+                }
+
+            });
+
+            reset.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onClick(View view) {
+                    // Add your code for the action
+                    amtInput.setText("");
+                    paxInput.setText("");
+                    SVC.setChecked(false);
+                    GST.setChecked(false);
+                    discInput.setText("");
+                    paymentRG.check(R.id.paymentCash);
+                    totalBill.setText("Total Bill: ");
+                    payEach.setText("Each Pays: ");
+
+
+                }
+            });
+
+
+        }
+    }
